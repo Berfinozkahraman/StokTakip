d88cd9bdd88f43d91579c28386c3dbb0a2516be6
berfi <berfi@BERFIN>
2023-10-12 14:36:32 +0300

    Add project files.

---
+++ StokTakip.WebApp/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StokTakip.WebApp.Models;
+
+namespace StokTakip.WebApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        private List<Product> _products;
+        public ProductController()
+        {
+            _products = new List<Product>()
+            {
+              new Product{Id=1, Name= "Msi", Description="Oyun bilgisayarı", Price=25000, Stock=25},
+              new Product{Id=2, Name= "Monster", Description="Oyun bilgisayarı", Price=27000, Stock=23}
+            };
+        }
+        public IActionResult Anasayfa()
+        {
+            return View(_products);
+        }
+    }
+}
---
+++ StokTakip.WebApp/Repository/RepositoryBaglantisi.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StokTakip.WebApp.Models;
+
+namespace StokTakip.WebApp.Repository;
+
+public class RepositoryBaglantisi : DbContext
+{
+    public RepositoryBaglantisi(DbContextOptions opt) : base (opt)
+    {
+
+    }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("server =(localdb)\\MSSQLLocalDB; Database = Stok_db; Trusted_Connection=true ");
+    }
+    public DbSet<Product> Products { get; set; }
+
+}
